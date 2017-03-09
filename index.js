8ade0f44110e2460d5fb3b3244e2e8ac15237971
jason chen <cjx89807@alibaba-inc.com>
2017-03-09 18:50:16 +0800

    feat: add some modules

--- index.js
+++
@@ -1,6 +0,0 @@
-
-export { Graphics } from './modules/core/display/Graphics';
-export { Vector3D } from './modules/core/display/TextFace';
-export { Camera } from './modules/filters/BlurFilter';
-
-export { Renderer } from './modules/core/Stage';
---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+export { _Math } from './math/Math';
+export { Color } from './math/Color';
+export { Ray } from './math/Ray';
+export { Vector4 } from './math/Vector4';
+export { Vector3 } from './math/Vector3';
+export { Camera } from './Camera';
+export { Scene } from './Scene';
+export { Renderer } from './Renderer';
